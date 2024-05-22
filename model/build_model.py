0403ac7f00ce7ea3a62d84aafcf4ef4aaf2dd135
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2024-05-22 15:06:41 +0700

    fix error path

--- model/build_model.py
+++ model/build_model.py
@@ -3,7 +3,7 @@ import importlib
 
 def build_model(data_handler):
     model_name = configs['model']['name']
-    module_path = ".".join(['models', model_name])
+    module_path = ".".join(['model', model_name])
     if importlib.util.find_spec(module_path) is None:
         raise NotImplementedError('Model {} is not implemented'.format(model_name))
     module = importlib.import_module(module_path)
